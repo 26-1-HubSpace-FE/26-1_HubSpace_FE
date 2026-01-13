532f76f4db06818d0284641bb210f5009178f163
leeeeeseojin <kkyuuu00@gmail.com>
2026-01-13 15:24:48 +0900

    feat: 사용자 조회 결과 페이지와 관리자 로그인 페이지 레이아웃 생성

--- src/pages/loginPage/entry/LoginPage.jsx
+++ src/pages/loginPage/entry/LoginPage.jsx
@@ -1,3 +1,23 @@
+import './LoginPage.css'
+
 export default function LoginPage() {
-  return <div>LoginPage</div>
+  return (
+    <div className='login'>
+      {/* HubSpace 로고 임시 */}
+      <h1 className='login__logo'>HubSpace</h1>
+
+      <h2 className='login__title'>관리자 로그인</h2>
+      <p className='login__description'>이벤트 관리 시스템에 접속하세요</p>
+
+      <div className='login__card'>
+        <p className='login__guide'>Google 계정으로 간편하게 로그인하세요</p>
+
+        {/* onClick={handleGoogleLogin */}
+        {/* 구글 아이콘 임시 */}
+        <button type='button' className='login__google-button'>
+          Google 계정으로 로그인
+        </button>
+      </div>
+    </div>
+  )
 }
