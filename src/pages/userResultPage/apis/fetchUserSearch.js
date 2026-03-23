66c30d54bfdd6d5135d6890cc71ed82b42272d80
holychan <east0321@outlook.kr>
2026-03-23 14:08:38 +0900

    refactor: 유저 조회 실패 시 404 에러 처리 변경

--- src/pages/userResultPage/apis/fetchUserSearch.js
+++ src/pages/userResultPage/apis/fetchUserSearch.js
@@ -17,7 +17,19 @@ export const fetchUserSearch = async (eventId, userSearchData) => {
     requestUrl,
   })
 
-  const res = await apiGetPublic(requestUrl)
-  console.debug('[UserSearch][Response]', res)
-  return res
+  try {
+    const res = await apiGetPublic(requestUrl)
+    console.debug('[UserSearch][Response]', res)
+    return res
+  } catch (err) {
+    // 백엔드에서 "응답 데이터 없음"을 404로 내려주는 경우
+    // 결과 페이지에서는 조회 실패(notFound)로 처리할 수 있도록 표준 응답 형태로 변환한다.
+    if (err?.response?.status === 404) {
+      const message = err?.response?.data?.message || '해당 정보로 조회된 기록이 없습니다.'
+      console.debug('[UserSearch][NotFound404]', { message })
+      return { isSuccess: false, message }
+    }
+
+    throw err
+  }
 }
