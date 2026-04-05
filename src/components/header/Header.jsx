60aaaf4f7ad1fd9426655a9e323660607539d11e
holychan <east0321@outlook.kr>
2026-04-05 09:08:00 +0900

    css: 기본 프로필 사진 표시 방식 변경

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
 import './Header.css'
-import { Icon } from '../icon/Icon'
 import { useFetchUserInfo } from './apis/fetchUserInfo'
 import { useNavigate } from 'react-router-dom'
 import defaultLogo from '../../assets/default/default-logo.svg'
+import defaultProfile from '../../assets/default/default-profile.jpg'
 
 export default function Header() {
   const { userInfo, loading } = useFetchUserInfo()
@@ -19,7 +19,7 @@ export default function Header() {
         <div className='header-title__title'>HubSpace</div>
       </button>
       <div className='header-account'>
-        <Icon name='default-profile' width={60} height={60} className='header-accout__profile' />
+        <img src={defaultProfile} alt='기본 프로필' className='header-account__profile' />
         <div className='header-account__info'>
           <div className='header-account__nickname'>{loading ? '불러오는 중...' : displayName}</div>
           <div className='header-account__email'>{loading ? '' : email}</div>
