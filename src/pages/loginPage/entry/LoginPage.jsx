b6b8ddc9dfd6c8008fb1db287920632e317caa1f
holychan <east0321@outlook.kr>
2026-04-08 12:16:14 +0900

    chore: 랜딩페이지 설명 추가

--- src/pages/loginPage/entry/LoginPage.jsx
+++ src/pages/loginPage/entry/LoginPage.jsx
@@ -12,8 +12,8 @@ export default function LoginPage() {
     <GradientLayout>
       <img src={authLogo} alt='HubSpace' className='login__logo' />
 
-      <h2 className='login__title'>관리자 로그인</h2>
-      <p className='login__description'>Google 계정으로 간편하게 로그인하세요</p>
+      <h2 className='login__title'>폼 신청 여부를 쉽게 확인할 수 있는 서비스</h2>
+      <p className='login__description'>신청자는 자신의 신청 여부를 확인하고, 관리자는 문의를 줄일 수 있습니다.</p>
 
       <button onClick={handleLogin} className='login__button'>
         <img src={googleIcon} />
