34da9360b289ccdc860f95928593d67c87cc3f31
holychan <east0321@outlook.kr>
2026-06-22 13:44:43 +0900

    fix: 랜딩 페이지에 설명 추가

--- src/pages/loginPage/entry/LoginPage.jsx
+++ src/pages/loginPage/entry/LoginPage.jsx
@@ -24,7 +24,9 @@ export default function LoginPage() {
       <img src={authLogo} alt='HubSpace' className='login__logo' />
 
       <h2 className='login__title'>폼 신청 여부를 쉽게 확인할 수 있는 서비스</h2>
-      <p className='login__description'>신청자는 자신의 신청 여부를 확인하고, 관리자는 문의를 줄일 수 있습니다.</p>
+      <p className='login__description'>신청자는 자신의 신청 여부를 확인하고, 관리자는 문의를 줄일 수 있습니다.<br/>
+        HubSpace helps event managers create Google Forms for event registration<br/>
+        and allows applicants to check whether their registration was successfully submitted.</p>
 
       <button onClick={handleLogin} className='login__button'>
         <img src={googleIcon} />
