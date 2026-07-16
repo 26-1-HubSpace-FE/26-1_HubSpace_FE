397b585d551a1fc95a4e393844e824e920967ca6
holychan <east0321@outlook.kr>
2026-07-17 02:40:38 +0900

    css: 로고 파일 변경 및 아이콘 제거

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,6 @@ import './Header.css'
 import { useFetchUserInfo } from './apis/fetchUserInfo'
 import { useNavigate } from 'react-router-dom'
 import hubspaceLogo from '../../assets/default/hubspace-logo.png'
-import defaultProfile from '../../assets/default/default-profile.png'
 
 export default function Header() {
   const { userInfo, loading } = useFetchUserInfo()
@@ -18,7 +17,6 @@ export default function Header() {
         <img src={hubspaceLogo} alt='HubSpace' className='header-title__logo' />
       </button>
       <div className='header-account'>
-        <img src={defaultProfile} alt='기본 프로필' className='header-account__profile' />
         <div className='header-account__info'>
           <div className='header-account__nickname'>{loading ? '불러오는 중...' : displayName}</div>
           <div className='header-account__email'>{loading ? '' : email}</div>
